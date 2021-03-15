280bf442b02a16a18728b1587480884795940e0e
au617797 <201805053@post.au.dk>
2021-03-15 16:09:57 +0100

    Worked on Lagrange Interpolation

--- Party/SSS.go
+++ Party/SSS.go
@@ -1,32 +1,63 @@
 package party
 
-import fields "MPC/Fields"
+import (
+	fields "MPC/Fields"
+	"errors"
+	"math"
+)
 
 type ShamirSecretSharing struct {
-	field fields.Field
-	poly  []int64
+	field  fields.Field
+	poly   []int64
+	degree int
+}
+
+type Share struct {
+	Value  int64
+	Number int64
 }
 
 func makeShamirSecretSharing(secret int64, fieldImp fields.Field, degree int) *ShamirSecretSharing {
 	SSS := new(ShamirSecretSharing)
+	SSS.degree = degree
 	SSS.field = fieldImp
-	SSS.poly = make([]int64, degree)
+	SSS.poly = make([]int64, degree+1)
 	SSS.poly[0] = secret
 	//Fill the rest of the polynomial with random values from the field
-	for i := 1; i < degree; i++ {
+	for i := 1; i <= degree; i++ {
 		SSS.poly[i] = SSS.field.GetRandom()
 	}
 	return SSS
 }
 
-func (s ShamirSecretSharing) lagrangeInterpolation() {
-
+func (s ShamirSecretSharing) lagrangeInterpolation(shares []Share) (secret int64, err error) {
+	if !(len(shares) > s.degree) {
+		return int64(0), errors.New("Lagrange: too few shares received")
+	}
+	result := float64(0)
+	//Computation as showed in example in MPC-book page 42
+	for i := 0; i <= s.degree; i++ {
+		enumerator := float64(shares[i].Value)
+		denominator := float64(1)
+		for j := 0; j <= s.degree; j++ {
+			if j != i {
+				enumerator = enumerator * float64(-shares[j].Number)
+				denominator = denominator * float64(shares[i].Number-shares[j].Number)
+			}
+		}
+		result += enumerator / denominator
+	}
+	return s.field.Convert((int64(math.Round(result)))), nil
 }
 
-func (s ShamirSecretSharing) sendShares() {
-
-}
-
-func (s ShamirSecretSharing) makeShares() {
-
+func (s ShamirSecretSharing) makeShares(numberOfParties int64) (shares []Share) {
+	for i := 1; i <= int(numberOfParties); i++ {
+		share := new(Share)
+		share.Number = int64(i)
+		for j, v := range s.poly {
+			share.Value = s.field.Add(share.Value, s.field.Multiply(v, s.field.Pow(int64(i), int64(j))))
+		}
+		shares = append(shares, *share)
+	}
+	return
 }
--- Party/SSS_test.go
+++ Party/SSS_test.go
@@ -2,26 +2,54 @@ package party
 
 import (
 	fields "MPC/Fields"
-	"fmt"
 	"testing"
 )
 
 //Testing the setup of SSS, with field Mod Prime
-func TestSSS(t *testing.T) {
+func TestModPrime(t *testing.T) {
 	field := fields.MakeModPrime(43)
 	degree := 10
 	SSS := makeShamirSecretSharing(42, field, degree)
-	fmt.Println(SSS.poly)
-	if len(SSS.poly) != degree {
-		t.Errorf("The length of poly does not match degree")
+	if len(SSS.poly) != degree+1 {
+		t.Errorf("The length of poly does not match degree, expected %v, got %v", degree+1, len(SSS.poly))
 	}
 	if SSS.poly[0] != 42 {
 		t.Errorf("The polynomial does not have the secret as the constant")
 	}
 	for i := 1; i < degree; i++ {
-		if SSS.poly[i] > 43 || SSS.poly[i] <= 0 {
+		if SSS.poly[i] >= 43 || SSS.poly[i] < 0 {
 			t.Errorf("The coeffecients in the polynomials are not in the range of the prime")
 		}
+	}
+}
 
+func TestShareMaking(t *testing.T) {
+	SSS := makeShamirSecretSharing(42, fields.MakeModPrime(43), 1)
+	SSS.poly = []int64{4, 7, 9, 1, 11}
+	shares := SSS.makeShares(5)
+	if shares[0].Value != (4+7+9+1+11)%43 {
+		t.Errorf("First share not computed correctly got %v expected %v", shares[0].Value, (4+7+9+1+11)%43)
+	}
+	if shares[1].Value != (4+7*2+9*4+1*8+11*16)%43 {
+		t.Errorf("First share not computed correctly got %v expected %v", shares[1].Value, (4+7*2+9*4+1*8+11*16)%43)
+	}
+	if shares[2].Value != (4+7*3+9*9+1*27+11*81)%43 {
+		t.Errorf("First share not computed correctly got %v expected %v", shares[2].Value, (4+7*3+9*9+1*27+11*81)%43)
+	}
+	if shares[3].Value != (4+7*4+9*16+1*64+11*256)%43 {
+		t.Errorf("First share not computed correctly got %v expected %v", shares[3].Value, (4+7*4+9*16+1*64+11*256)%43)
+	}
+	if shares[4].Value != (4+7*5+9*25+1*125+11*625)%43 {
+		t.Errorf("First share not computed correctly got %v expected %v", shares[4].Value, (4+7*5+9*25+1*125+11*625)%43)
+	}
+}
+
+func TestLagrangeInterpolation(t *testing.T) {
+	secret := int64(42)
+	SSS := makeShamirSecretSharing(secret, fields.MakeModPrime(43), 10)
+	shares := SSS.makeShares(11)
+	secretLagrange, _ := SSS.lagrangeInterpolation(shares)
+	if secretLagrange != secret {
+		t.Errorf("Wrong secret, got %v expected %v", secretLagrange, secret)
 	}
 }
