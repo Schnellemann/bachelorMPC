9b55ec79d3a45434c31d84e6a004b533d0a57aaf
au617797 <201805053@post.au.dk>
2021-03-22 23:12:14 +0100

    Wrote tests for parseExpression and made a few corrections, 1 test still does not pass, result naming is not working correctly

--- Config/ParseExpression.go
+++ Config/ParseExpression.go
@@ -18,52 +18,69 @@ func parseExpression(exp string) ast.Expr {
 
 }
 
-func isValid(holdings []holding) error {
+func isValid(instructions []instruction) error {
 	//TODO
 	return nil
 }
 
-//Returns result identifier and a list of holdings which are the operations that needs to be computed in the given order
+//Returns result identifier and a list of instructions which are the operations that needs to be computed in the given order
 //returns convertErr if conversion failed
-func convertAstToExpressionList(exp ast.Expr) (string, []holding, error) {
-	resNum, holdings, err := convertAstAux(1, exp)
-	isValidErr := isValid(holdings)
+func convertAstToExpressionList(exp ast.Expr) (string, []instruction, error) {
+	resNum, instructions, err := convertAstAux(1, exp)
+	isValidErr := isValid(instructions)
 	if isValidErr != nil {
 		return "", nil, isValidErr
 	}
-	return "r" + strconv.Itoa(resNum), holdings, err
+	return "r" + strconv.Itoa(resNum), instructions, err
 }
 
-func convertAstAux(unique int, exp ast.Expr) (int, []holding, error) {
+func convertAstAux(unique int, exp ast.Expr) (int, []instruction, error) {
 	switch exp := exp.(type) {
 	case *ast.BinaryExpr:
 		return convertBinaryExpr(unique, exp)
+	case *ast.BasicLit, *ast.Ident:
+		return 0, nil, nil
+	case *ast.ParenExpr:
+		return convertAstAux(unique, exp.X)
 	default:
-		return 0, nil, fmt.Errorf("Failed to convert ast to holdings list")
+		return 0, nil, fmt.Errorf("Failed to convert ast to instructions list")
 	}
 }
 
-func convertBinaryExpr(unique int, exp *ast.BinaryExpr) (int, []holding, error) {
-	leftUnique, leftHoldings, err := convertAstAux(unique, exp.X)
+func convertBinaryExpr(unique int, exp *ast.BinaryExpr) (int, []instruction, error) {
+	leftUnique, leftInstructions, err := convertAstAux(unique, exp.X)
 	if err != nil {
 		return 0, nil, err
 	}
-	rightUnique, rightHoldings, err := convertAstAux(leftUnique, exp.Y)
+	rightUnique, rightInstructions, err := convertAstAux(leftUnique, exp.Y)
 	if err != nil {
 		return 0, nil, err
 	}
 	unique = rightUnique + 1
 	op := exp.Op
-	var binaryIns holding
+	var binaryIns instruction
 	switch op {
 	case token.ADD:
-		binaryIns = createAddition(getExprName(exp.X, leftUnique), getExprName(exp.X, rightUnique), "r"+strconv.Itoa(unique))
+		binaryIns = createAddition(getExprName(exp.X, leftUnique), getExprName(exp.Y, rightUnique), "r"+strconv.Itoa(unique))
 	case token.MUL:
-		binaryIns = createMultiplication(getExprName(exp.X, leftUnique), getExprName(exp.X, rightUnique), "r"+strconv.Itoa(unique))
+		//Need to see if its scale by constant or multiply
+		switch x := exp.X.(type) {
+		case *ast.BasicLit:
+			binaryIns = createScalar(x.Value, getExprName(exp.Y, rightUnique), "r"+strconv.Itoa(unique))
+		default:
+			switch y := exp.Y.(type) {
+			case *ast.BasicLit:
+				binaryIns = createScalar(y.Value, getExprName(exp.X, rightUnique), "r"+strconv.Itoa(unique))
+			default:
+				//None were basic lits so mul
+				binaryIns = createMultiplication(getExprName(exp.X, leftUnique), getExprName(exp.Y, rightUnique), "r"+strconv.Itoa(unique))
+			}
+		}
+
 	}
-	holdings := append(leftHoldings, binaryIns)
-	holdings = append(holdings, rightHoldings...)
-	return unique, holdings, nil
+	instructions := append(leftInstructions, binaryIns)
+	instructions = append(instructions, rightInstructions...)
+	return unique, instructions, nil
 }
 
 func getExprName(node ast.Expr, lastUnique int) string {
---
+++ Config/ParseExpression_test.go
@@ -0,0 +1,106 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestParseExpressionAdd(t *testing.T) {
+	toConvert := "p1+p2"
+	exp := parseExpression(toConvert)
+	res, instructions, _ := convertAstToExpressionList(exp)
+	if len(instructions) != 1 {
+		t.Errorf("Number of instructions incorrect got %v expected %v", len(instructions), 1)
+	}
+	if instructions[0].op != add {
+		t.Errorf("Wrong instruction got %v expected %v", instructions[0].op, add)
+	}
+	if instructions[0].left != "p1" {
+		t.Errorf("Wrong instruction left side got %v expected %v", instructions[0].left, "p1")
+	}
+	if instructions[0].right != "p2" {
+		t.Errorf("Wrong instruction right side got %v expected %v", instructions[0].right, "p2")
+	}
+	if instructions[0].result != res {
+		t.Errorf("Result did not match returned %v and created %v", res, instructions[0].result)
+	}
+}
+
+func TestParseExpressionMul(t *testing.T) {
+	toConvert := "p1*p2"
+	exp := parseExpression(toConvert)
+	res, instructions, _ := convertAstToExpressionList(exp)
+	if len(instructions) != 1 {
+		t.Errorf("Number of instructions incorrect got %v expected %v", len(instructions), 1)
+	}
+	if instructions[0].op != multiply {
+		t.Errorf("Wrong instruction got %v expected %v", instructions[0].op, multiply)
+	}
+	if instructions[0].left != "p1" {
+		t.Errorf("Wrong instruction left side got %v expected %v", instructions[0].left, "p1")
+	}
+	if instructions[0].right != "p2" {
+		t.Errorf("Wrong instruction right side got %v expected %v", instructions[0].right, "p2")
+	}
+	if instructions[0].result != res {
+		t.Errorf("Result did not match returned %v and created %v", res, instructions[0].result)
+	}
+}
+
+func TestParseExpressionScalar(t *testing.T) {
+	toConvert := "1*p1"
+	exp := parseExpression(toConvert)
+	res, instructions, _ := convertAstToExpressionList(exp)
+	if len(instructions) != 1 {
+		t.Errorf("Number of instructions incorrect got %v expected %v", len(instructions), 1)
+	}
+	if instructions[0].op != scalar {
+		t.Errorf("Wrong instruction got %v expected %v", instructions[0].op, scalar)
+	}
+	if instructions[0].left != "1" {
+		t.Errorf("Wrong instruction left side got %v expected %v", instructions[0].left, "1")
+	}
+	if instructions[0].right != "p1" {
+		t.Errorf("Wrong instruction right side got %v expected %v", instructions[0].right, "p1")
+	}
+	if instructions[0].result != res {
+		t.Errorf("Result did not match returned %v and created %v", res, instructions[0].result)
+	}
+}
+
+func TestParseExpressionCombined(t *testing.T) {
+	toConvert := "(1*p1+p2)*p3"
+	exp := parseExpression(toConvert)
+	res, instructions, _ := convertAstToExpressionList(exp)
+	if len(instructions) != 3 {
+		t.Errorf("Number of instructions incorrect got %v expected %v", len(instructions), 3)
+	}
+	if instructions[0].op != scalar {
+		t.Errorf("Wrong third instruction got %v expected %v", instructions[0].op, scalar)
+	}
+	if instructions[1].op != add {
+		t.Errorf("Wrong third instruction got %v expected %v", instructions[1].op, add)
+	}
+	if instructions[2].op != multiply {
+		t.Errorf("Wrong third instruction got %v expected %v", instructions[2].op, multiply)
+	}
+
+	if instructions[2].result != res {
+		t.Errorf("Results did not match returned %v and created %v", res, instructions[0].result)
+	}
+	if instructions[0].result == instructions[1].result || instructions[0].result == instructions[2].result || instructions[1].result == instructions[2].result {
+		t.Errorf("Overlapping results")
+	}
+	if instructions[2].result != "r3" {
+		t.Errorf("Result from multiply is %v expected %v", instructions[2].result, "r3")
+	}
+	if instructions[1].result != "r2" {
+		t.Errorf("Result from add is %v expected %v", instructions[1].result, "r2")
+	}
+	if instructions[0].result != "r1" {
+		t.Errorf("Result from scalar is %v expected %v", instructions[0].result, "r1")
+	}
+}
+
+func TestIsValid(t *testing.T) {
+
+}
