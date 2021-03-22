9bacb80a84439f42e36c9bd8890524b4050b4d2f
au617797 <201805053@post.au.dk>
2021-03-22 14:23:42 +0100

    Better plan for parseExpression, still not done

--- Config/ParseExpression.go
+++ Config/ParseExpression.go
@@ -18,63 +18,59 @@ func parseExpression(exp string) ast.Expr {
 
 }
 
+func isValid(holdings []holding) error {
+	//TODO
+	return nil
+}
+
 //Returns result identifier and a list of holdings which are the operations that needs to be computed in the given order
 //returns convertErr if conversion failed
 func convertAstToExpressionList(exp ast.Expr) (string, []holding, error) {
 	resNum, holdings, name, err := convertAstAux(1, exp)
+	isValidErr := isValid(holdings)
+	if isValidErr != nil {
+		return 0, nil, isValidErr
+	}
 	return name, holdings, err
 }
 
-func convertAstAux(unique int, exp ast.Expr) (int, []holding, string, error) {
+func convertAstAux(unique int, exp ast.Expr) (int, []holding, error) {
 	switch exp := exp.(type) {
 	case *ast.BinaryExpr:
 		return convertBinaryExpr(unique, exp)
 	default:
-		fmt.Printf("Failed to convert ast to holdings list")
-		return 0, nil
+		return 0, nil, fmt.Errorf("Failed to convert ast to holdings list")
 	}
-	return 0, nil
 }
 
-
-func convertBinaryExpr(unique int, exp *ast.BinaryExpr) (int, []holding, string, error) {
+func convertBinaryExpr(unique int, exp *ast.BinaryExpr) (int, []holding, error) {
+	leftUnique, leftHoldings, err := convertAstAux(unique, exp.X)
+	if err != nil {
+		return 0, nil, err
+	}
+	rightUnique, rightHoldings, err := convertAstAux(leftUnique, exp.Y)
+	if err != nil {
+		return 0, nil, err
+	}
+	unique = rightUnique + 1
 	op := exp.Op
-		switch op {
-		case token.ADD:
-			unique, leftHoldings, err := convertAstAux(unique, exp.X)
-			if err != nil {
-				return 0, nil, err
-			}
-			unique, rightHoldings, name, err := convertAstAux(unique, exp.Y)
-			addition := createAddition(x.Name, "r"+strconv.Itoa(num))
-			switch x := exp.X.(type) {
-			case *ast.Ident:
-				switch y := exp.Y.(type) {
-				case *ast.Ident:
-					addition = createAddition(x.Name, y.Name)
-				default:
-					//y is a general expression so use r[num]
-					num, rightHoldings, err := 
-					unique = num + 1
-					
-					return unique, append([]holding{addition}, rightHoldings...), err
-				}
-			default:
-				//X is not Ident
-				switch y := exp.Y.(type) {
-				case *ast.Ident:
-					num, leftHoldings, err := convertAstAux(unique, exp.X)
-					unique = num + 1
-					addition = createAddition("r"+strconv.Itoa(num), y.Name)
-					return unique, append(leftHoldings, addition), err
-				default:
+	var binaryIns holding
+	switch op {
+	case token.ADD:
+		binaryIns = createAddition(getExprName(exp.X, left), getExprName(exp.X, right), "r"+strconv.Itoa(unique))
+	case token.MUL:
+		binaryIns = createMultiplication(getExprName(exp.X, left), getExprName(exp.X, right), "r"+strconv.Itoa(unique))
+	}
+	holdings := append(leftHoldings, binaryIns)
+	holdings = append(holdings, rightHoldings...)
+	return unique, holdings, nil
+}
 
-					return 0, nil, fmt.Errorf("Failed to convert ast to holdings list in addition")
-				}
-
-			}
-		case token.MUL:
-			
-			return 0, nil
-		}
+func getExprName(node ast.Expr, lastUnique int) string {
+	switch t := node.(type) {
+	case *ast.Ident:
+		return t.Name
+	default:
+		return "r" + strconv.Itoa(lastUnique)
+	}
 }
