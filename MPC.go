8fe9c8fa1adbe686235639d1bde4ee6b94c9e76c
au617797 <201805053@post.au.dk>
2021-05-03 13:51:59 +0200

    Setup main for running protocol

--- MPC.go
+++ MPC.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	config "MPC/Config"
 	exp "MPC/Experiment"
+	field "MPC/Fields"
 	graph "MPC/Graph"
+	p "MPC/Party"
+	prot "MPC/Protocol"
+	"fmt"
 )
 
 func main() {
+	path := "Config/configFiles/" + "kaare" + ".json"
+	config := &config.ReadConfig(path)[0]
+	field := field.MakeModPrime(1049)
+	peer := p.MkPeer(config)
+	protocol := prot.MkProtocol(config, field, peer)
+	res := protocol.Run()
+	fmt.Printf("Got result: %v\n", res)
+
+}
+
+func runExperiments() {
 	e := graph.MkExcel()
 	exp.IncDelay(e)
 	e = graph.MkExcel()
