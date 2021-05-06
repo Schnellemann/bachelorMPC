074ea189cc915e49c1d1108d83dbd2fe8b7f6421
Jens Scheel Nellemann <jens.scheel.nellemann@gmail.com>
2021-05-06 10:37:14 +0200

    Added the bandwidth experiment, and peers+bandwidth experiment

---
+++ Experiment/IncBandwidth.go
@@ -0,0 +1,61 @@
+package experiment
+
+import (
+	config "MPC/Config"
+	field "MPC/Fields"
+	graph "MPC/Graph"
+	party "MPC/Party"
+	prot "MPC/Protocol"
+	"fmt"
+	"time"
+)
+
+func IncBandwidth(plotter graph.Interface) {
+	fieldRange := 1049
+	plotter.NewSeries("Bandwidth size")
+	for i := 20; i < 200; i *= 2 {
+		fmt.Printf("Starting Experiment with %v width. \n", i)
+		secretList := makeRandomSecretList(10, fieldRange)
+		expression := makeRandomMultExpression(len(secretList), i)
+
+		configs := config.MakeConfigs(ip, expression, secretList)
+		peerlist := getXPeers(configs)
+		var bandwidthPeerlist []party.IPeer
+		//Convert to bandwidthPeer
+		for j, p := range peerlist {
+			bPeer := party.MkBandwidthPeer(configs[j], p, i, 2*time.Millisecond)
+			bandwidthPeerlist = append(bandwidthPeerlist, bPeer)
+		}
+		var channels []chan int64
+		var timers []*prot.Times
+		for j := 0; j < i; j++ {
+			timers = append(timers, new(prot.Times))
+		}
+		var tProtList []*prot.Times
+		for j, c := range configs {
+			channel := make(chan int64)
+			channels = append(channels, channel)
+			//Make protocol
+			p := prot.MkProtocol(c, field.MakeModPrime(int64(fieldRange)), bandwidthPeerlist[j])
+			tprot := prot.MkTimeMeasuringProt(p, c, timers[j])
+			tProtList = append(tProtList, tprot.Timer)
+			go goProt(tprot, channel)
+			time.Sleep(100 * time.Millisecond)
+		}
+
+		//Change this so it checks that all the results are similar
+		var resultList []int
+		for _, c := range channels {
+			result := <-c
+			resultList = append(resultList, int(result))
+		}
+
+		if !allSameResults(resultList) {
+			fmt.Println("Peers do not agree on the result")
+			fmt.Printf("Result: %v \n", resultList)
+		}
+		avgTProt := prot.AverageTimes(tProtList)
+		plotter.AddData(i, avgTProt)
+	}
+	plotter.Plot()
+}
