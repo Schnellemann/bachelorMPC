d5d3112a3fd999767b521d654837c8cb9d3fb9c5
Jens Scheel Nellemann <jens.scheel.nellemann@gmail.com>
2021-06-03 10:03:35 +0200

    Added some to read me

--- MPC.go
+++ MPC.go
@@ -8,11 +8,18 @@ import (
 	p "MPC/Party"
 	prot "MPC/Protocol"
 	"fmt"
-	"strconv"
+	"os"
 )
 
 func main() {
-	runExperiments()
+	args := os.Args[1:]
+	if args[0] == "" {
+		fmt.Println("Running Experiment")
+		runExperiments()
+	}
+	if args[0] == "files" {
+		makeDistributed()
+	}
 }
 
 func runConfig() {
@@ -27,28 +34,46 @@ func runConfig() {
 
 func runExperiments() {
 	var e graph.Interface
-	//e = graph.MkExcel("Increment Peer", "Peers")
-	//exp.IncrementPeer(e)
+
+	/*
+		Increasing peer
+	*/
+	e = graph.MkExcel("Increment Peer", "Peers")
+	exp.IncrementPeer(e)
+
+	/*
+		Increasing amount of multiplications and peer delay
+	*/
 	//e = graph.MkExcel("Increment-Mult+Delay", "Number of mults")
 	//exp.IncrementMultAndDelay(e)
+
+	/*
+		Increasing amount of multiplications
+	*/
 	//e = graph.MkExcel("Increment-Mult", "Delay (ms)")
 	//exp.IncrementMult(e)
+
+	/*
+		Increasing peer delay
+	*/
 	//e = graph.MkPlotter("Increment Delay", "", "png", "Number of Peers")
 	//exp.IncDelay(e)
-	numberOfMults := 14000
-	e = graph.MkExcel("Distributed-Mult-"+strconv.Itoa(numberOfMults), "Mults")
-	computerNr := 1
 
-	path := "com_" + strconv.Itoa(computerNr) + "-" + strconv.Itoa(numberOfMults) + "-mults.json"
-	exp.RunDistributedExperiment(path, e, numberOfMults)
+	/*
+		Distributed experiment increment Peers
+	*/
+	// numberOfMults := 1000
+	// computerNr := 1
+	// e = graph.MkExcel("Distributed-Mult-"+strconv.Itoa(numberOfMults), "Mults")
+	// path := "com_" + strconv.Itoa(computerNr) + "-" + strconv.Itoa(numberOfMults) + "-mults.json"
+	// exp.RunDistributedExperiment(path, e, numberOfMults)
 
 }
 
 func makeDistributed() {
-	var ips = []string{
-		"192.168.1.193",
-		"192.168.1.141",
-		"192.168.1.248",
-	}
+	/*
+		Insert Ip's here to create config files for distributed experiment
+	*/
+	var ips = []string{}
 	exp.MakeDistributedIncMults(1000, 20000, 1000, ips)
 }
